e2ee745f01d61ff8495c6fbe85d0c6e4b9599468
Balicea24 <bralicea@syr.edu>
2019-05-21 23:50:37 -0400

    First Commit

---
+++ BitstampLitecoin.py
@@ -0,0 +1,82 @@
+import pysher
+import logging
+import sys
+import time
+import psycopg2
+import json
+from datetime import datetime
+
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+ch = logging.StreamHandler(sys.stdout)
+root.addHandler(ch)
+
+conn = psycopg2.connect(database="postgres", user = "postgres", password = "Roflmao24!", host = "127.0.0.1", port = "5432")
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE TRADES
+      (ID           SERIAL PRIMARY KEY,
+      UNIXTIMESTAMP            INT     NOT NULL,
+      PRICE        FLOAT(8),
+      AMOUNT         FLOAT(8),
+      TYPE          INT     NOT NULL,
+      BUY_ID            BIGINT NOT NULL,
+      SELL_ID            BIGINT NOT NULL);''')
+
+cur.execute('''CREATE TABLE ORDERS
+     (ID               SERIAL PRIMARY KEY,
+      ORDER_ID            BIGINT     NOT NULL,
+      UNIXTIMESTAMP            INT     NOT NULL,
+      PRICE        FLOAT(8),
+      AMOUNT         FLOAT(8),
+      TYPE          INT     NOT NULL);''')
+
+conn.commit()
+
+class Trades:
+    pusher = pysher.Pusher("de504dc5763aeef9ff52", daemon = False)
+    def __init__(self):
+        self.pusher.connection.bind('pusher:connection_established', self.listen)
+        self.pusher.connect()
+
+    def listen(self, data):
+        channel = self.pusher.subscribe('live_trades_ltcusd')
+        channel.bind('trade', self.get)
+
+    def get(self, *args):
+        jsonify = args[0].split("'")[0]
+        data = json.loads(jsonify)
+        self.push_to_DB(data)
+
+    def push_to_DB(self, data):
+        unixtimestamp = int(data["datetime"])
+        price = data['price_str']
+        amount = data['amount']
+        type = data['type']
+        buy_id = data['buy_order_id']
+        sell_id = data['sell_order_id']
+
+        cur.execute("INSERT INTO TRADES (UNIXTIMESTAMP,PRICE,AMOUNT,TYPE, BUY_ID, SELL_ID) \
+              VALUES (%s, %s, %s, %s, %s, %s)", (unixtimestamp, price, amount, type, buy_id, sell_id));
+
+        conn.commit()
+
+class Orders(Trades):
+    pusher = pysher.Pusher("de504dc5763aeef9ff52", daemon = False)
+    def listen(self, data):
+        channel = self.pusher.subscribe('live_orders_ltcusd')
+        channel.bind('order_created', self.get)
+
+    def push_to_DB(self, data):
+        unixtimestamp = int(data["datetime"])
+        price = data["price"]
+        amount = data["amount"]
+        type = data["order_type"]
+        order_id = data['id']
+
+        cur.execute("INSERT INTO ORDERS (ORDER_ID,UNIXTIMESTAMP,PRICE,AMOUNT,TYPE) \
+              VALUES (%s, %s, %s, %s, %s)", (order_id, unixtimestamp, price, amount, type));
+
+        conn.commit()
+
+Trades(), Orders()
